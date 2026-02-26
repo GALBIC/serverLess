40a3c6652bfcc2865bee0cb7e087a90f293c1b2a
GALBIC <braxisemiller@gmail.com>
2026-02-26 20:06:49 +0330

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import app from "./api/index.js";
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  console.log(`Test: http://localhost:${PORT}/depth/BTC`);
-});
